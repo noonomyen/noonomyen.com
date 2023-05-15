99d9dfd287ac9733d8338a98739e07e18a922480
noonomyen <nomyen@noonomyen.com>
2023-05-15 15:27:00 +0700

    replace flag abbreviations with full names

--- backend/proxy/cloudflare/block-direct-connect/ip6tables.sh
+++ backend/proxy/cloudflare/block-direct-connect/ip6tables.sh
@@ -3,13 +3,12 @@
 # Cloudflare IP Ranges
 # Last updated: April 8, 2021
 
-ip6tables --append INPUT --protocol tcp --source 2400:cb00::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2606:4700::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2803:f800::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2405:b500::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2405:8100::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2a06:98c0::/29 -m multiport --destination-ports 80,443 --jump ACCEPT
-ip6tables --append INPUT --protocol tcp --source 2c0f:f248::/32 -m multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2400:cb00::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2606:4700::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2803:f800::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2405:b500::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2405:8100::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2a06:98c0::/29 --match multiport --destination-ports 80,443 --jump ACCEPT
+ip6tables --append INPUT --protocol tcp --source 2c0f:f248::/32 --match multiport --destination-ports 80,443 --jump ACCEPT
 
-ip6tables --append INPUT --protocol tcp -m multiport --destination-ports 80,443 --jump DROP
-
+ip6tables --append INPUT --protocol tcp --match multiport --destination-ports 80,443 --jump DROP
