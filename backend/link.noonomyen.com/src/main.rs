03f2c1dbede2d8ed313941e51b8f52614fa90753
noonomyen <nomyen@noonomyen.com>
2023-05-21 04:47:45 +0700

    redirect-server (backend) support UDS (IPC socket)
    add unix domain socket for bind connection (localhost to localhost)

--- backend/link.noonomyen.com/src/main.rs
+++ backend/link.noonomyen.com/src/main.rs
@@ -5,27 +5,48 @@ use std::process::exit;
 mod map;
 mod server;
 
+fn help(exit_code: i32, args: &Vec<String>) {
+    let exec_name = Path::new(&args[0]).file_name().unwrap().to_str().unwrap();
+    println!("{} http [ip] [port] [list file]", exec_name);
+    println!("{} uds [uds path] [list file]", exec_name);
+    println!("");
+    println!("NAME: {}", env!("CARGO_PKG_NAME"));
+    println!("VERSION: {}", env!("CARGO_PKG_VERSION"));
+    println!("DESCRIPTION: {}", env!("CARGO_PKG_DESCRIPTION"));
+    exit(exit_code);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
 
-    if !(args.len() == 4) {
-        println!("{} [ip] [port] [list file]", Path::new(&args[0]).file_name().unwrap().to_str().unwrap());
-        println!("");
-        println!("NAME: {}", env!("CARGO_PKG_NAME"));
-        println!("VERSION: {}", env!("CARGO_PKG_VERSION"));
-        println!("DESCRIPTION: {}", env!("CARGO_PKG_DESCRIPTION"));
-        exit(1);
+    if args_len == 1 {
+        help(0, &args)
     }
 
-    let ip = &args[1];
-    let port = args[2].parse::<u16>().unwrap();
-    let fp: &Path = Path::new(&args[3]);
-    if !fp.exists() {
-        println!("'{}' file not found", args[1]);
-        exit(1);
+    if args[1] == "http" && args_len == 5 {
+        let ip = &args[2];
+        let port = args[3].parse::<u16>().unwrap();
+        let fp: &Path = Path::new(&args[4]);
+        if !fp.exists() {
+            println!("'{}' file not found", &args[1]);
+            exit(1);
+        } else {
+            let map = map::new(fp);
+            let _ = server::start(server::ServerMode::HTTP { ip, port }, map);
+        }
+    } else if args[1] == "uds" && args_len == 4 {
+        let uds_path: &Path = Path::new(&args[2]);
+        let fp: &Path = Path::new(&args[3]);
+        if !fp.exists() {
+            println!("'{}' file not found", &args[1]);
+            exit(1);
+        } else {
+            let map = map::new(fp);
+            let _ = server::start(server::ServerMode::UDS { uds_path }, map);
+        }
     } else {
-        let map = map::new(fp);
-        let _ = server::start(ip, port, map);
+        help(1, &args);
     }
 
     exit(0);
--- backend/link.noonomyen.com/src/server.rs
+++ backend/link.noonomyen.com/src/server.rs
@@ -1,5 +1,11 @@
+use std::path::Path;
 use std::collections::HashMap;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use actix_web::{ web, App, HttpResponse, HttpServer, Responder, HttpRequest };
+
+pub enum ServerMode<'lf> {
+    HTTP { ip: &'lf str, port: u16 },
+    UDS { uds_path: &'lf Path },
+}
 
 async fn index() -> impl Responder {
     return HttpResponse::Ok()
@@ -20,11 +26,14 @@ async fn redirector(req: HttpRequest, data_map: web::Data::<HashMap<String, Stri
 }
 
 #[actix_web::main]
-pub async fn start(ip: &str, port: u16, map: HashMap<String, String>) -> std::io::Result<()> {
-    HttpServer::new(move || App::new()
+pub async fn start(bind: ServerMode, map: HashMap<String, String>) -> std::io::Result<()> {
+    let app = move || App::new()
         .app_data(web::Data::new(map.clone()))
         .route("/", web::get().to(index))
-        .default_service(web::get().to(redirector))
-    )
-    .bind((ip, port))?.run().await
+        .default_service(web::get().to(redirector));
+
+    match bind {
+        ServerMode::HTTP { ip, port } => HttpServer::new(app).bind((ip, port))?.run().await,
+        ServerMode::UDS { uds_path } => HttpServer::new(app).bind_uds(uds_path)?.run().await
+    }
 }
